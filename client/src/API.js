42be8b5e94c32d3a9a08003f7c82b82fc9652789
GioPom <s304868@studenti.polito.it>
2022-10-15 16:11:19 +0200

    Login interface complete, need a backend to test it. API to use login/logout finished.

---
+++ client/src/API.js
@@ -0,0 +1,48 @@
+const URL = "http://localhost:3001/"
+
+//USED TO GET INFO ABOUT QUEE FROM SERVER
+async function getAllInfos(){
+    const response = await fetch(URL);
+    const services = await response.json();
+    if(response.ok){
+        return services.map((c) => ({id:c.id, info1:c.info1, info2:c.info2, info3:c.info1info3}))
+    } else {
+        throw services;
+    }
+}
+
+  async function login(credentials) {
+    let response = await fetch(URL+'sessions', {
+      method: 'POST',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(credentials),
+    });
+    
+    if (response.ok) {
+      const user = await response.json();
+      return user;
+    } else {
+      const errDetail = await response.json();
+      throw errDetail.message;
+    }
+  }
+  
+  async function logout() {
+    await fetch(URL+'sessions/current', { method: 'DELETE', credentials: 'include' });
+  }
+  
+  async function getUserInfo() {
+    const response = await fetch(URL+'sessions/current', {credentials: 'include'});
+    const userInfo = await response.json();
+    if (response.ok) {
+      return userInfo;
+    } else {
+      throw userInfo;  // an object with the error coming from the server
+    }
+  }
+
+const API = {getAllInfos,login,logout, getUserInfo};
+export default API;
--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
+import './custom.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import API from './API';
+import { Container, Row } from 'react-bootstrap';
+import { BrowserRouter as Router, Routes, Route, Navigate, useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { LoginForm } from './components/login';
+import MyNavbar2 from './components/navbarlogin';
 
-function App() {
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <App2/>
+    </Router>
+  )
+}
+
+function App2() {
+  const [loggedIn,setLoggedIn]=useState(false);
+  const [user, setUser] = useState({});
+  const [message, setMessage] = useState('');
+  
+  const navigate = useNavigate();
+
+  const doLogout = async () => {
+    await API.logout();
+    setLoggedIn(false);
+    setUser({});
+    /*
+    setFirsttime(true);
+    setDirty(true);
+    i'll see after if ill need it
+    */
+    navigate('/');
+  }
+
+  // used to login
+  const login = ()=>{navigate("/login")}
+  const doLogin = (credentials) => {
+    API.login(credentials)
+      .then( user => {
+        setLoggedIn(true);
+        setUser(user);
+        //setFirsttime(true);
+        setMessage('');
+        navigate('/officer');
+      })
+      .catch(err => {
+        setMessage(err);
+      }
+        )
+  }
+
+  return (
+    <>
+    <MyNavbar2 loggedIn={loggedIn} logout={doLogout} login={login}/>
+    <Container fluid>
+       <Row className="vheight-100">
+            <Routes> 
+               <Route path='/' /*element={(<Home cc={cc} setCc = {setCc} piano={piano} setPiano={setPiano} updateUser={updateUser} loggedIn={loggedIn} deleteUser={deleteUser} */></Route>
+               <Route path='/login'  element={loggedIn ? <Navigate to='/officer' /> : <LoginForm login={doLogin} loginError={message} setLoginError={setMessage} /> }/>
+               <Route path='/officer'  element={loggedIn ? <Navigate to='/officer' /> : <Navigate to='/' />}/>
+            </Routes>
+       </Row>
+    </Container>
+    </>
+    );
+  
 }
 
 export default App;
