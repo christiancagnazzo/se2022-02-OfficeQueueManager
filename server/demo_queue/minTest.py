61bbc9ffae5e4e6579250f574e873717aa03f8f1
kangchenghua <chenghuakang@gmail.com>
2022-10-16 23:08:00 +0200

    Update minTest.py

--- server/demo_queue/minTest.py
+++ server/demo_queue/minTest.py
@@ -12,18 +12,18 @@ class TestMinWaitingtime(unittest.TestCase):
         self.assertEqual("15:50",waitTime)
         
     def test2(self):
-        t_r = 30 #service time is 5 minutes
+        t_r = 30 #service time is 30 minutes
         n_r = 40 #40 people waiting for the same service,
-        k_i = [2,2,1] # 3 service center offer 2 and 1 kind of service respectively
+        k_i = [2,2,1] # 3 service center offer 2,2 and 1 kind of service respectively
         s_i_r = [1,1,1] # all service offer this kind of service
         waitTime = minWaitingTime(t_r,n_r,k_i,s_i_r)
         self.assertEqual("615:00",waitTime)
         
     def testCatchError(self):
-        t_r = 30 #service time is 5 minutes
+        t_r = 30 #service time is 30 minutes
         n_r = 40 #40 people waiting for the same service,
-        k_i = [2,2,1] # 3 service center offer 2 and 1 kind of service respectively
-        s_i_r = [0,0,0] # all service offer this kind of service
+        k_i = [2,2,1] # 3 service center offer 2,2 and 1 kind of service respectively
+        s_i_r = [0,0,0] # no service offer this kind of service
         with self.assertRaises(ZeroDivisionError):
             waitTime = minWaitingTime(t_r,n_r,k_i,s_i_r)
             
