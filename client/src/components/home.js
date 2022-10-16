0ce9a845c1db8301cfb7760e2a54f4703d403c51
GioPom <s304868@studenti.polito.it>
2022-10-16 19:20:29 +0200

    finished request ticket, need api and restoring page

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,27 +1,46 @@
-import { Col, ListGroup, Button } from 'react-bootstrap';
-import { useState, useEffect } from 'react'
+import { Button,ButtonGroup } from 'react-bootstrap';
+
 
 
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 function Home(props) {
-console.log(props.services.length)
+
     return (props.services.length!==0 ?
         <>
-        
-            <Col md={12} className="col-12 below-nav"> 
-                <ImgSet services={props.services}></ImgSet>
-            </Col>
-            <Col md={12} >
-            ciao2    
-            </Col>
+        <ButtonGroup >
+            <Button type="button"  variant="primary" onClick={() => {clicking(0,props.updateQueue)}}>{ButtonSet(0,props)}</Button>
+            <Button type="button"  variant="secondary" onClick={() => {clicking(1,props.updateQueue)}}>{ButtonSet(1,props)}</Button>
+            <Button type="button"  variant="success" onClick={() => {clicking(2,props.updateQueue)}}>{ButtonSet(2,props)}</Button>
+        </ButtonGroup>
+        <ButtonGroup >
+            <Button type="button"  variant="warning" onClick={() => {clicking(3,props.updateQueue)}}>{ButtonSet(3,props)}</Button>
+            <Button type="button"  variant="danger" onClick={() => {clicking(4,props.updateQueue)}}>{ButtonSet(4,props)}</Button>
+            <Button type="button"  variant="light" onClick={() => {clicking(5,props.updateQueue)}}>{ButtonSet(5,props)}</Button>
+        </ButtonGroup>        
         </>:
         <h1 className="col-12 below-nav">Hey! non riceviamo alcuna risposta dal server! Prova a riaggiornare la pagina!</h1>
         
     );
 }
-function ImgSet(props){
+function ButtonSet(n,props){
+// PROPS HAVE SERVICES WITH REMAINING QUE FOR SERVICE
+/*
+let x = props.services[n]
+let y =props.queue[n]
+return (<>
+    <div>{x}</div>
+    <div>queue: {y}</div>
+    </>)
+*/
+    return (<>
+    <div>servizio</div>
+    <div>coda</div>
+    </>)
+}
 
+function clicking(n,updateQueue){
+    updateQueue(n)
 }
 
 export default Home;
